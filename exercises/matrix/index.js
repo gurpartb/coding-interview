2214ec7f150ba0524af99fd9ccdb811db66791ed
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-02-13 15:10:18 -0700

    matrix challenge completed.

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,50 +15,67 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
+// n size of row / column
+// k counter
+// M matrix
+// i row index (start)
+// j column index (start)
+// x row index (current)
+// y colument index (current)
+
+/*
+*
+*
+*
+*
+*/
+
+
+
+// top row
 function move_right(n, k, M, i, j){
-  for(y = j; y < j+n; y++){
+  for(let y = j; y < j+n; y++){
     M[i][y] = k++;
   }
-  if(n==0){
+  if(n==0){ // base case when n is even
     M[i][j] = k;
-  }
-  if(n<=0){
     return;
   }
   return move_down(n, k, M, i, j+n);
 }
 
+// right column
 function move_down(n, k, M, i, j){
-  for(x = i; x < i+n; x++){
+  for(let x = i; x < i+n; x++){
     M[x][j] = k++;
   }
-  if(n<=0){
+  if(n==-1){ // base case when n is odd
     return;
   }
   return move_left(n, k, M, i+n, j);
 }
 
+// bottom row
 function move_left(n, k, M, i, j){
-  for(y = j; y > j-n; y-- ){
+  for(let y = j; y > j-n; y-- ){
     M[i][y] = k++;
   }
   return move_up(n, k, M, i, j-n);
 }
 
+// left column
 function move_up(n, k, M, i, j){
-  for(x = i; x > i-n; x--){
+  for(let x = i; x > i-n; x--){
     M[x][j] = k++;
   }
+  // n-2 , re
   return move_right(n-2, k, M, i-n+1, j+1);
 }
 
 function matrix(n) {
   var M = []
-  for(i=0; i<n;i++){
+  for(let i=0; i<n;i++){
     let m = []
-    for(j=0; j<n; j++){
-      m.push(0);
-    }
     M.push(m);
   }
   move_right(n-1, 1, M, 0, 0);
