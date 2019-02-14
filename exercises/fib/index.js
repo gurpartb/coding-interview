2c928d80f02d0618743732f8640667c91cc15d1c
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-02-13 17:12:55 -0700

    fib memoize solution.

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,61 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoize(fn){
+    const cache = {};
+    return function(...args){
+        if(cache[args]){
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+function slowFib(n){
+    if(n<2){
+        return n;
+    }
+    return fib(n-1) + fib(n-2)
+}
+
+const fib = memoize(slowFib);
+
+console.log(fib(7));
 
 module.exports = fib;
+
+
+// var fibMem = [0,1];
+
+// function fib(n) {
+//     if(n <= fibMem.length - 1 ){
+//         return fibMem[n];
+//     }
+//     fibMem[n] = fib(n-1) + fib(n-2);
+//     return fibMem[n];
+// }
+
+// module.exports = fib;
+
+// function fib(n) {
+//     if(n <= 1){
+//         return n;
+//     }
+//     return fib(n-1) + fib(n-2);
+// }
+
+// module.exports = fib;
+
+// function fib(n) {
+//     var res = [0,1];
+//     for(let i = 2; i <= n; i++){
+//         res.push(res[i-2] + res[i-1]);
+//     }
+//     return res[n];
+// }
+
+// module.exports = fib;
