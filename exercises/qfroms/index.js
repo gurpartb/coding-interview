45d18fe63176b7bd43880ec25b2fa2ac23c6f620
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-02-20 14:38:02 -0700

    finished qforms tutorial

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,32 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+
+    constructor(){
+        this.stackPrimary = new Stack();
+        this.stackHelper = new Stack();
+    }
+
+    add(record){
+        // push everything to stackHelper
+        while(this.stackPrimary.peek()){
+            this.stackHelper.push(this.stackPrimary.pop());
+        }
+        this.stackPrimary.push(record);
+        // push everything to stackPrimary
+        while(this.stackHelper.peek()){
+            this.stackPrimary.push(this.stackHelper.pop());
+        }
+    }
+
+    remove(){
+        return this.stackPrimary.pop();
+    }
+
+    peek(){
+        return this.stackPrimary.peek();
+    }
+}
 
 module.exports = Queue;
