81be9dc817201cc6e2f40671397d9dc89f6e89fa
gurpartb <gurpartap.s.bhatti@gmail.com>
2019-03-09 20:33:16 -0700

    finished validate tutorial

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,26 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+    if(min !== null && min > node.data){
+
+        return false
+
+    }
+    if(max !== null && max < node.data){
+
+        return false
+
+    }
+    if(node.left){
+
+        return validate(node.left, min, node.data)
+    }
+    if(node.right){
+
+        return validate(node.right, node.data, max)
+    }
+    return true
+}
 
 module.exports = validate;
